256b4f2ac58c4ec1a67671e9473011494d236d10
COVEO\lvu <lvu@coveo.com>
2026-03-23 11:54:09 -0400

    Update navigation bar

--- navbar.js
+++ navbar.js
@@ -1,8 +1,8 @@
 document.querySelector("#nav-bar").innerHTML = `
     <div id="navbar-container">
-      <a href="/">&#x2605; SearchAPI</a>
+      <a href="/">&#x2605; Conversational</a>
       <a href="/answerApi/">&#x2605; AnswerAPI</a> 
-      <a href="/conversational/">&#x2605; Conversational</a> 
+      <a href="/conversational/">&#x2605; SearchAPI</a> 
   
     </div>
   `;
