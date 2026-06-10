abb14325efc41fed8de7d0a652f5ca5a6f6b922c
COVEO\lvu <lvu@coveo.com>
2026-06-10 09:22:31 -0400

    Update menu bar

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ export default {
     rollupOptions: {
       input: {
         main: "index.html",
+        advancedSearch: "advancedSearch/index.html",
+        contextSearch: "contextSearch/index.html",
+        staticFilter: "staticFilter/index.html",
         answerApi: "answerApi/index.html",
         searchApi: "searchApi/index.html",
         conversationalProd: "conversationalProd/index.html",
